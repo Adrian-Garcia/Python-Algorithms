2c70c38b41639815ae000b88fbd8a523f94a7c52
Adrian-Garcia <adriangarlop@hotmail.com>
2020-05-27 17:25:19 -0500

    Added and accepted twoSum.py at leetcode

---
+++ vector/twoSum.py
@@ -0,0 +1,31 @@
+"""
+Two Sum
+
+	Given an array of integers, return indices of the two numbers such that they add up to a specific 
+	target.
+	
+	You may assume that each input would have exactly one solution, and you may not use the same
+	element twice.
+
+Example:
+	Given nums = [2, 7, 11, 15], target = 9,
+
+	Because nums[0] + nums[1] = 2 + 7 = 9,
+	return [0, 1].
+"""
+
+class Solution(object):
+
+	def twoSum(self, nums, target):
+
+		dictionary = {}
+
+		for i in range(len(nums)):
+
+			if (target - nums[i] in dictionary):
+				return [dictionary[target - nums[i]], i]
+
+			dictionary[nums[i]] = i		
+
+sol = Solution()
+print(sol.twoSum([1,2,3], 5))
