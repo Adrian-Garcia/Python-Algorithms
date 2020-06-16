f6f2d8545ea8d204aca56fe97eb57e5d90bfe67c
Adrian-Garcia <adriangarlop@hotmail.com>
2020-06-15 22:14:26 -0500

    Added structure at graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -1,1 +1,14 @@
+class GraphNode(object):
+
+	def __init__ (self, val):
+		self.val = val
+		self.adjacent = {}
+
 class Graph():
+
+	def __init__ (self, start):
+		self.start = start
+
+	def breadthFirst(self):
+
+		
