ff313331cbedfa1331963348c56d068bb688babf
Adrian-Garcia <adriangarlop@hotmail.com>
2020-06-15 20:31:38 -0500

    Added list definitions

---
+++ vector/listsMatching.py
@@ -0,0 +1,14 @@
+arr1 = [1,2,3,4]
+arr2 = arr1
+arr3 = arr1[:]
+
+print("Arr1 = [1,2,3,4] = {}".format(arr1))
+print("Arr2 = Arr1 = {}".format(arr2))
+print("Arr3 = Arr1[:] = {}".format(arr3))
+
+arr1.remove(4)
+
+print("\nArr1.remove(4)\n")
+print("Arr1 = {}".format(arr1))
+print("Arr2 = {}".format(arr2))
+print("Arr3 = {}".format(arr3))
