20db8012c0283ca043e3c9555056fff608d48fd5
Adrian-Garcia <adriangarlop@indexzero.io>
2020-09-29 13:41:09 -0500

    Added semaphores.py

---
+++ semaphores/semaphores.py
@@ -0,0 +1,22 @@
+import threading
+import time
+sem = threading.Semaphore()
+
+def fun1():
+    while True:
+        sem.acquire()
+        print(1)
+        sem.release()
+        time.sleep(0.25) # this can be ignored
+
+def fun2():
+    while True:
+        sem.acquire()
+        print(2)
+        sem.release()
+        time.sleep(0.25) # this can be ignored
+
+t = threading.Thread(target = fun1)
+t.start()
+t2 = threading.Thread(target = fun2)
+t2.start()
