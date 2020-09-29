7ab820fa693702775fcad115b4f1fd37630fe651
Adrian-Garcia <adriangarlop@indexzero.io>
2020-09-29 13:56:26 -0500

    Added counter.py

--- tricks/scopeGlobalVariable.py
+++ tricks/scopeGlobalVariable.py
@@ -2,31 +2,31 @@ myGlobalVar = 3009
 
 # not using function
 def testOfGlobalVar():
-	print("My myGlobalVar = ", myGlobalVar)
+	print("My myGlobalVar = ", myGlobalVar) # 3009
 
 # this is totaly going to crash
 def chanteGlobalVar(): 
 	myGlobalVar += 1
-	print("myGlobalVar = ", myGlobalVar)
+	print("myGlobalVar = ", myGlobalVar) # Error
 
 # this is not going to crash, is the correct way of doing it
 def changeGlobalVarAsParameter(myGlobalVar): 
 	myGlobalVar += 1
-	print("myGlobalVar = ", myGlobalVar)
+	print("myGlobalVar = ", myGlobalVar) # 3010
 
 # this is going to crash because copy is not available in an integer
 def copyVarToChange(myGlobalVar):
 	copiedVar = myGlobalVar.copy()
 	copiedVar += 1
-	print("copiedVar = ", copiedVar)
+	print("copiedVar = ", copiedVar) # Error
 
 # this is the correct way of doing it
 def equalVarToChange():
 	equaledVar = myGlobalVar
 	equaledVar += 1
 
-	print("myGlobalVar = ", myGlobalVar)
-	print("equaledVar = ", equaledVar)	
+	print("myGlobalVar = ", myGlobalVar) # 3009
+	print("equaledVar = ", equaledVar)	 # 3010
 
 # testOfGlobalVar()
 # changeGlobalVarAsParameter(myGlobalVar)
