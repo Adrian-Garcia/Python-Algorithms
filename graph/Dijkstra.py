829262b65c3d396615ead77ea1321087f574096f
Adrian-Garcia <adriangarlop@indexzero.io>
2020-09-29 13:13:26 -0500

    Added Dijkstra

--- graph/Dijkstra.py
+++ graph/Dijkstra.py
@@ -1,22 +1,22 @@
 import heapq
 
-def calculate_distances(graph, starting_vertex):
+def calculate_distances(graph, start):
     distances = {vertex: float('infinity') for vertex in graph}
-    distances[starting_vertex] = 0
+    distances[start] = 0
 
-    pq = [(0, starting_vertex)]
-    while len(pq) > 0:
-        current_distance, current_vertex = heapq.heappop(pq)
+    priorityQueue = [(0, start)]
+    while len(priorityQueue):
+        currDist, currVertex = heapq.heappop(priorityQueue)
 
-        if current_distance > distances[current_vertex]:
+        if currDist > distances[currVertex]:
             continue
 
-        for neighbor, weight in graph[current_vertex].items():
-            distance = current_distance + weight
+        for neighbor, weight in graph[currVertex].items():
+            distance = currDist + weight
 
             if distance < distances[neighbor]:
                 distances[neighbor] = distance
-                heapq.heappush(pq, (distance, neighbor))
+                heapq.heappush(priorityQueue, (distance, neighbor))
 
     return distances
 
