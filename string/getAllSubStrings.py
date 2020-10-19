679aa681d28ca5a5389d7090607af902b36aac9c
Adrian-Garcia <adriangarlop@hotmail.com>
2020-10-18 21:01:42 -0500

    Added getAllSubElements from list

--- string/getAllSubStrings.py
+++ string/getAllSubStrings.py
@@ -9,5 +9,9 @@ def get_all_substrings(string):
 def get_all_substrings_one_line(str):
 	return [str[i:j+1] for i in range(len(str)) for j in range(i, len(str))]
 
+def get_all_sublists(list):
+	return [list[i:j+1] for i in range(len(list)) for j in range(i, len(list))]
+
 print(get_all_substrings('abcde'))
 print(get_all_substrings_one_line('abcde'))
+print(get_all_sublists([1,2,3,4,5]))
