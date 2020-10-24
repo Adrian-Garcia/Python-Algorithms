9b7216a81a1e1f8d91ea3675e67606adae22bfb9
Adrian-Garcia <adriangarlop@indexzero.io>
2020-10-24 11:40:26 -0500

    Implemented aPriori algorythm

--- machineLearning/aPriori.py
+++ machineLearning/aPriori.py
@@ -1,5 +1,20 @@
-def aPriori():
-	print("ajua")
-	pass
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
 
-aPriori()
+transactions = [['milk', 'bread', 'eggs'],
+				['milk', 'juice'],
+				['juice', 'butter'],
+				['milk', 'bread', 'eggs'],
+				['coffe', 'eggs'],
+				['coffe'],
+				['coffe', 'juice'],
+				['milk', 'bread', 'cookies', 'eggs'],
+				['cookies', 'butter'],
+				['milk', 'bread']]
+
+te = TransactionEncoder()
+te_ary = te.fit(transactions).transform(transactions)
+df = pd.DataFrame(te_ary, columns=te.columns_)
+
+print(apriori(df, min_support=0.3))
