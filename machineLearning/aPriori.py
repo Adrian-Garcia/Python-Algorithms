225dfe23721d3dfab8e362b3ad717b1bbed43638
Adrian-Garcia <adriangarlop@indexzero.io>
2020-10-24 16:18:21 -0500

    Added kMeans structure

--- machineLearning/aPriori.py
+++ machineLearning/aPriori.py
@@ -7,6 +7,7 @@ import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori
 
+minimum_support = 0.3
 transactions = [['milk', 'bread', 'eggs'],
 				['milk', 'juice'],
 				['juice', 'butter'],
@@ -23,8 +24,8 @@ te_ary = te.fit(transactions).transform(transactions)
 df = pd.DataFrame(te_ary, columns=te.columns_)
 print(df, end="\n\n")
 
-frequent_itemsets = apriori(df, min_support=0.3, use_colnames=True)
-frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x: len(x))
+frequent_itemsets = apriori(df, min_support=minimum_support, use_colnames=True)
+frequent_itemsets['iteracion'] = frequent_itemsets['itemsets'].apply(lambda x: len(x))
 frequent_itemsets
 
 print(frequent_itemsets)
---
+++ machineLearning/kMeans.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+
+# Read dataset
+# data = pd.read_csv('3D_spatial_network.csv', header=None)
+data = pd.read_csv('transactions.csv', header=None)
+
+# Drop first column (not required)
+# data.drop(labels=0, axis=1, inplace=True)
+
+# Normalize data (min/max scaling)
+data_arr = data.values
+sc = preprocessing.MinMaxScaler()
+data_sc = sc.fit_transform(data_arr)
+data = pd.DataFrame(data_sc)
+
+print(data)
