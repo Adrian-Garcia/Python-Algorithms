1aa5a740bc4f6f9614cf01cd1c37bea8266ea409
Adrian-Garcia <adriangarlop@indexzero.io>
2020-10-24 11:52:55 -0500

    Got a perfect implementation for apriori algoryth

--- machineLearning/aPriori.py
+++ machineLearning/aPriori.py
@@ -1,3 +1,8 @@
+''' 
+	Recovered from
+	https://stackabuse.com/association-rule-mining-via-apriori-algorithm-in-python/
+'''
+
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori
@@ -16,5 +21,10 @@ transactions = [['milk', 'bread', 'eggs'],
 te = TransactionEncoder()
 te_ary = te.fit(transactions).transform(transactions)
 df = pd.DataFrame(te_ary, columns=te.columns_)
+print(df, end="\n\n")
 
-print(apriori(df, min_support=0.3))
+frequent_itemsets = apriori(df, min_support=0.3, use_colnames=True)
+frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x: len(x))
+frequent_itemsets
+
+print(frequent_itemsets)
