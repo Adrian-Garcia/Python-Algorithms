28a7be348e44505f88dc676ded8f74d9c9e7c897
Adrian-Garcia <adriangarlop@hotmail.com>
2023-12-01 22:57:03 -0600

    Verified graphClass DFS and BFS

--- graph/graphClass.py
+++ graph/graphClass.py
@@ -1,7 +1,7 @@
 """
 Data Structure for graphs. 
-  - DFS need to be verified 
-  - BFS need to be verified 
+  - DFS 
+  - BFS 
 """
 
 
@@ -12,7 +12,7 @@ class Node(object):
 
     def breadthFirstSearch(self):
         queue = [self]
-        visited = set()
+        visited = {self.val}
 
         return_nodes = list()
         return_values = list()
@@ -22,7 +22,6 @@ class Node(object):
 
             return_nodes.append(curr_node)
             return_values.append(curr_node.val)
-            visited.add(curr_node.val)
 
             for neighbor in curr_node.neighbors:
                 if neighbor.val not in visited:
@@ -33,7 +32,7 @@ class Node(object):
 
     def deepFirstSearch(self):
         stack = [self]
-        visited = set()
+        visited = {self.val}
 
         return_nodes = list()
         return_values = list()
@@ -43,7 +42,6 @@ class Node(object):
 
             return_nodes.append(curr_node)
             return_values.append(curr_node.val)
-            visited.add(curr_node.val)
 
             for neighbor in reversed(curr_node.neighbors):
                 if neighbor.val not in visited:
@@ -63,8 +61,32 @@ node2.neighbors = [node1, node3]
 node3.neighbors = [node2, node4]
 node4.neighbors = [node1, node3]
 
-return_nodes_bfs, return_values_bfs = node1.breadthFirstSearch()
+# return_nodes_bfs, return_values_bfs = node1.breadthFirstSearch()
+# print("return_values_bfs =", return_values_bfs)
+
+# return_nodes_dfs, return_values_dfs = node1.deepFirstSearch()
+# print("return_values_dfs =", return_values_dfs)
+
+n0 = Node(0)
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
+n6 = Node(6)
+n7 = Node(7)
+
+n0.neighbors = [n3, n4]
+n1.neighbors = [n2, n5, n6]
+n2.neighbors = [n1, n5]
+n3.neighbors = [n0, n5, n7]
+n4.neighbors = [n0, n6]
+n5.neighbors = [n1, n2, n3, n7]
+n6.neighbors = [n1, n4, n7]
+n7.neighbors = [n3, n5, n6]
+
+return_nodes_bfs, return_values_bfs = n0.breadthFirstSearch()
 print("return_values_bfs =", return_values_bfs)
 
-return_nodes_dfs, return_values_dfs = node1.deepFirstSearch()
+return_nodes_dfs, return_values_dfs = n0.deepFirstSearch()
 print("return_values_dfs =", return_values_dfs)
