7af901441893462adb6f9750dba4103dc0f82c25
Adrian-Garcia <adriangarlop@hotmail.com>
2024-11-27 18:05:02 -0600

    Added more leetcode excersises

--- interviews/listopro/subMatrixOfVowels.py
+++ interviews/listopro/subMatrixOfVowels.py
@@ -1,4 +1,4 @@
-'''
+"""
 You are given an n x m matrix.
 
 Your job is ot find if there is a sub-matrix of 2x2 that contains all vowels
@@ -17,34 +17,35 @@ example_two = [
   ['klmno'],
 ]
 result = not found
-'''
+"""
+
 
 def allVowelsInSubStrArr(subStrArr):
-  vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+    vowels = ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
 
-  for i in range(len(subStrArr)):
-    for j in range(len(subStrArr[i])):
-      if subStrArr[i][j] not in vowels:
-        return False
+    for i in range(len(subStrArr)):
+        for j in range(len(subStrArr[i])):
+            if subStrArr[i][j] not in vowels:
+                return False
 
-  return True
-  
+    return True
 
 
 def MatrixChallenge(strArr):
-  for i in range(len(strArr)):
-    for j in range(len(strArr[i])):
+    for i in range(len(strArr)):
+        for j in range(len(strArr[i])):
 
-      if i + 1 < len(strArr) and j + 1 < len(strArr[i]):
-        subStrArr = [
-          [strArr[i][j], strArr[i][j + 1]],
-          [strArr[i + 1][j], strArr[i + 1][j + 1]]
-        ]
+            if i + 1 < len(strArr) and j + 1 < len(strArr[i]):
+                subStrArr = [
+                    [strArr[i][j], strArr[i][j + 1]],
+                    [strArr[i + 1][j], strArr[i + 1][j + 1]],
+                ]
 
-        if allVowelsInSubStrArr(subStrArr):
-          return str(i) + "-" + str(j)
+                if allVowelsInSubStrArr(subStrArr):
+                    return str(i) + "-" + str(j)
 
-  return "not found"
+    return "not found"
 
-# keep this function call here 
+
+# keep this function call here
 print(MatrixChallenge(input()))
